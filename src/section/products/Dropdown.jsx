9739cf26cc088775ea5e32e363853848739177e1
Md Ramjan Ali <mdramjan.ict@gmail.com>
2025-03-23 08:04:01 +0600

    ramjan

--- src/section/products/Dropdown.jsx
+++ src/section/products/Dropdown.jsx
@@ -2,10 +2,16 @@ import React from "react";
 
 const Dropdown = ({ list }) => {
   return (
-    <div className="">
-      {list?.map((item, i) => {
-        <p key={i}>{item}</p>;
-      })}
+    <div className="p-2 ml-4 border-l border-gray-200 ">
+      <p className="cursor-pointer transition-all  hover:bg-[#F4F4F5] py-1 px-2 rounded-md ">
+        ramjan
+      </p>
+      <p className="cursor-pointer transition-all  hover:bg-[#F4F4F5] py-1 px-2 rounded-md ">
+        ramjan
+      </p>
+      <p className="cursor-pointer transition-all  hover:bg-[#F4F4F5] py-1 px-2 rounded-md ">
+        ramjan
+      </p>
     </div>
   );
 };
