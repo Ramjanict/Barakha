279f39bd739421650c6374cbdaec2331c0e22f7c
Ramjan <mdramjan.ict@gmail.com>
2025-05-06 06:26:30 +0600

    ramjan

--- admin/src/components/Sidebar.jsx
+++ admin/src/components/Sidebar.jsx
@@ -1,23 +1,28 @@
-import { Home, Settings, User, LogOut } from "lucide-react";
+import { TbWaveSawTool } from "react-icons/tb";
+import { RiBuilding2Line } from "react-icons/ri";
+import { IoStatsChart } from "react-icons/io5";
+import { LuBoxes } from "react-icons/lu";
+import { MdLogout } from "react-icons/md";
+
+const sidebarMenu = [
+  { label: "Overview", path: "/", icon: TbWaveSawTool },
+  { label: "Sales", path: "/", icon: IoStatsChart },
+  { label: "Products", path: "/", icon: LuBoxes },
+  { label: "Offices", path: "/", icon: RiBuilding2Line },
+  { label: "Logout", path: "/", icon: MdLogout },
+];
 
 const Sidebar = () => {
   return (
     <div className="flex flex-col h-screen py-2 bg-white w-60">
-      <nav className="flex flex-col gap-2 ">
-        <SidebarItem icon={<Home size={20} />} label="Home" />
-        <SidebarItem icon={<User size={20} />} label="Profile" />
-        <SidebarItem icon={<Settings size={20} />} label="Settings" />
-        <SidebarItem icon={<LogOut size={20} />} label="Logout" />
+      <nav className="flex flex-col gap-4 ">
+        {sidebarMenu.map((item, i) => (
+          <div className="flex items-center gap-1 px-4 py-1 cursor-pointer hover:bg-gray-50">
+            <span className="text-2xl">{<item.icon />}</span>
+            <p>{item.label}</p>
+          </div>
+        ))}
       </nav>
-    </div>
-  );
-};
-
-const SidebarItem = ({ icon, label }) => {
-  return (
-    <div className="flex items-center gap-2 p-2 transition-colors rounded cursor-pointer hover:bg-gray-100">
-      {icon}
-      <span>{label}</span>
     </div>
   );
 };
