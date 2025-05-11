b75df35c6eba58a1b78138568efb609a527c4ef0
Ramjan <mdramjan.ict@gmail.com>
2025-05-12 05:30:25 +0600

    ramjan

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,16 +1,4 @@
 import React from "react";
-<<<<<<< HEAD
-import Dashboard from "./pages/Dashboard";
-import { Route, Routes } from "react-router-dom";
-
-const App = () => {
-  return (
-    <div>
-      <Routes>
-        <Route path="/" element={<Dashboard />} />
-      </Routes>
-    </div>
-=======
 import AdminDashboard from "./pages/AdminDashboard";
 
 const App = () => {
@@ -18,7 +6,6 @@ const App = () => {
     <>
       <AdminDashboard />
     </>
->>>>>>> 71cf09d1cd487550be6216f1a4ba94c9cec97d3e
   );
 };
 
