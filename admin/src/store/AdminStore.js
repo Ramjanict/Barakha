d54da1d1c438eacb4d06daa7807a9c42e470b17e
Md Ramjan Ali <mdramjan.ict@gmail.com>
2025-06-02 14:43:49 +0600

    backend controller added

---
+++ admin/src/store/AdminStore.js
@@ -0,0 +1,51 @@
+import { create } from "zustand";
+import { persist } from "zustand/middleware";
+import axios from "axios";
+import { toast } from "react-toastify";
+
+// Secure Axios instance
+const axiosSecure = axios.create({
+  baseURL: import.meta.env.VITE_BACKEND,
+});
+
+export const useAdminStore = create(
+  persist(
+    (set, get) => ({
+      user: null,
+      token: "",
+      isLoading: false,
+
+      login: async (loginUser) => {
+        set({ isLoading: true });
+        try {
+          const response = await axiosSecure.post("/auth/login", loginUser);
+          const data = response.data;
+
+          if (data && data.data?.accessToken) {
+            set({ token: data.data.accessToken });
+            toast.success(data.message || "Login successful");
+          } else {
+            toast.error(data.message || "Login failed");
+          }
+        } catch (error) {
+          console.error("Problem during login:", error);
+          toast.error("Something went wrong. Please try again.");
+        } finally {
+          set({ isLoading: false });
+        }
+      },
+
+      logout: () => {
+        set({ token: "", user: null });
+        toast.success("Admin logged out successfully");
+      },
+    }),
+    {
+      name: "user", // Local storage key
+      partialize: (state) => ({
+        token: state.token,
+        user: state.user,
+      }),
+    }
+  )
+);
